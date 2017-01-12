587c719eb0e09242aae6b6b80e841a70bcb89ace
aimei <657074944@qq.com>
2017-01-12 16:30:42 +0800

    add my

--- demos/wuxiaobopindao/pages/my/my.js
+++ demos/wuxiaobopindao/pages/my/my.js
@@ -1,19 +1,28 @@
 // pages/my/my.js
+var app = getApp();
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+
   },
-  onReady:function(){
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    var that = this;
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+  },
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
