04fc9f1c468ceef2ebe0ec22396c46375926d4e6
aimeiyan <aimeiyan@gmail.com>
2017-06-06 17:41:44 +0800

    bk

--- demos/haoGuImitation/pages/bk/bk.js
+++ demos/haoGuImitation/pages/bk/bk.js
@@ -1,19 +1,26 @@
 // pages/bk/bk.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+    quotation: {},
+    goodsId: 2002004,
+    goodsName: '',
+    goodsCode: '',
+    isAddToZxg: false    // 是否已添加到自选股
   },
-  onReady:function(){
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    console.log(options,"---options-");
+  },
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
