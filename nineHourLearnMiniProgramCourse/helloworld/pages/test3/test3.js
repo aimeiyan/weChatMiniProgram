beeafb132f694ca8f920affb08447ecf4da147cf
Amy <657074944@qq.com>
2018-06-23 22:40:55 +0800

    practice

---
+++ nineHourLearnMiniProgramCourse/helloworld/pages/test3/test3.js
@@ -0,0 +1,83 @@
+Page({
+
+  clickA(){
+    console.log("click container A");
+  },
+  clickB(){
+    console.log("click container B");
+  },
+  clickC(){
+    console.log("click container C");
+  },
+  captureClickA(){
+    console.log("capture container A");
+  },
+  captureClickB(){
+    console.log("capture container B");
+  },
+  captureClickC(){
+    console.log("capture container C");
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
